ac201fd5f9fa935b612f4cc1f04a327a7f78997b
Jennifer Ngetich <jen@sabaitechnology.com>
2014-05-28 13:28:15 -0400

    updating auth and settings pages but still broken

--- www/bin/auth.php
+++ www/bin/auth.php
@@ -3,4 +3,5 @@ $userName=$_REQUEST['vpnaUsername'];
 $userPass=$_REQUEST['vpnaUserpass'];
 $password = crypt($userPass, base64_encode($userPass));
 file_put_contents("/var/www/sys/net.auth", $userName:$password)
+echo "res={ sabai: 1, msg: 'Credentials Updated' };";
 ?>
