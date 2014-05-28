3d553f19ce6d7cdf5b01d1941486d76e9647d343
Tim Fowler <tfowler@sabaitechnology.com>
2014-05-28 11:08:34 -0400

    add auth.php to change the username and password for http auth

---
+++ www/bin/auth.php
@@ -0,0 +1,6 @@
+<?php
+$userName=$_REQUEST['vpnaUsername'];
+$userPass=$_REQUEST['vpnaUserpass'];
+$password = crypt($userPass, base64_encode($userPass));
+file_put_contents("/var/www/sys/net.auth", $userName:$password)
+?>
