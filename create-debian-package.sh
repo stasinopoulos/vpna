d6aca7b712d3a03bb09c2009fdbc20e6b8713d9f
Kirk Fort <kirk@sabaitechnology.com>
2014-06-17 16:27:55 -0400

    package reset service, close #37

--- create-debian-package.sh
+++ create-debian-package.sh
@@ -3,5 +3,8 @@
 mkdir -p debian/var/www
 
 rsync -av --exclude-from '.gitignore' www debian/var/
+rsync -av reset/vpna_reset.conf debian/etc/init/
+rsync -av reset/vpna_reset.pl debian/home/sabai/
+
 
 dpkg-deb --build debian ./sabai-vpna.deb
