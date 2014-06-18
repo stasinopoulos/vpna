b0fe422467a8f7418b6ce3bc711f30ab694c6d70
Kirk Fort <kirk@sabaitechnology.com>
2014-06-18 11:38:18 -0400

    Write to syslog

--- reset/vpna_reset.pl
+++ reset/vpna_reset.pl
@@ -1,20 +1,25 @@
 #!/usr/bin/perl -w
 use Linux::Input;
+use Sys::Syslog;
+use Sys::Syslog qw(:standard :macros);
+
+openlog("VPNAreset", "", LOG_USER);
 
 sub reinstall_vpna () {
     for(`dpkg -i /home/sabai/sabai-vpna.deb`) {
 	printf;
     }
-    printf "DONE\n";
+    syslog ("notice", "VPNA reset completed");
 }
 
 $sleepbtn = Linux::Input->new('/dev/input/event1');
 
+syslog ('info', 'Starting VPNA reset service');
+
 while (1) {
     while (my $events = $sleepbtn->poll(.3)) {
-	printf "$events \n";
 	if ($events > 9000) {
-	    printf "RESET!\n";
+	    syslog('notice', "Initiating VPNA reset");
 	    reinstall_vpna();
 	}
     }
