0fd3ba2f02b537310e5cc45cfec6e9266937ee5a
Kirk Fort <kirk@sabaitechnology.com>
2014-06-18 08:10:14 -0400

    packaging fix for clean install

--- create-debian-package.sh
+++ create-debian-package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p debian/var/www
+mkdir -p debian/etc/init
+mkdir -p debian/home/sabai
 
 rsync -av --exclude-from '.gitignore' www debian/var/
 rsync -av reset/vpna_reset.conf debian/etc/init/
