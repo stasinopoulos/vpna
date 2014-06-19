d4f9f559ea2017cae3d4c7a30b3b0dfb7007a719
Kirk Fort <kirk@sabaitechnology.com>
2014-06-19 10:13:09 -0400

    restore vpn on reboot

--- create-debian-package.sh
+++ create-debian-package.sh
@@ -5,7 +5,7 @@ mkdir -p debian/etc/init
 mkdir -p debian/home/sabai
 
 rsync -av --exclude-from '.gitignore' www debian/var/
-rsync -av reset/vpna_reset.conf debian/etc/init/
+rsync -av reset/*.conf debian/etc/init/
 rsync -av reset/vpna_reset.pl debian/home/sabai/
 
 
