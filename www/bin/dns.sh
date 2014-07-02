d742edc9f5eaf3e00a6aad1928edf0456df63232
Tim Fowler <tim@roobixx.com>
2014-07-02 08:46:30 -0400

    Remove interfaces changes (blanks it break on reboot)

--- www/bin/dns.sh
+++ www/bin/dns.sh
@@ -7,20 +7,20 @@ nameserver $dns1
 nameserver $dns2
 EOF
 
-#change for later
-cat > /etc/network/interfaces << EOF
-# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
-
-# The loopback network interface
-auto lo
-iface lo inet loopback
-
-# The primary network interface
-auto eth0
-iface eth0 inet dhcp
-dns-nameservers $dns1 $dns2
-EOF
+##change for later
+#cat > /etc/network/interfaces << EOF
+## This file describes the network interfaces available on your system
+## and how to activate them. For more information, see interfaces(5).
+#
+3# The loopback network interface
+#auto lo
+#iface lo inet loopback
+#
+## The primary network interface
+#auto eth0
+#iface eth0 inet dhcp
+#dns-nameservers $dns1 $dns2
+#EOF
 
 touch /var/www/stat/dns
 
