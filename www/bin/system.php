07c83a7fefc9560a4aaca8c6615207390da79141
William Haynes <william@sabaitechnology.com>
2014-11-18 17:33:30 -0500

    Update system.php
    sudo didn't have a command.  Now it sh's.

--- www/bin/system.php
+++ www/bin/system.php
@@ -6,7 +6,7 @@ if(isset($_REQUEST['act']) && $_REQUEST['act']!="")
 $act=$_REQUEST['act'];
 
 
-$toShell= exec("sudo /var/www/bin/system.sh $act",$out);
+$toShell= exec("sudo sh /var/www/bin/system.sh $act",$out);
 
 echo $toShell;
 
