3a6049ad47e76fe1695cd1782a9111dceb944594
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2015-11-11 09:42:23 +0200

    Added Input Data Validation.

--- www/bin/shell.php
+++ www/bin/shell.php
@@ -6,23 +6,23 @@ switch($act){
   $ip = $_REQUEST['ip'];
   $count = $_REQUEST['count'];
   $size = $_REQUEST['size'];
-  $ex="ping $ip -c $count";
+  $ex="ping ".escapeshellcmd($ip)." -c ".escapeshellcmd($count);
  break; }
  case 2:
   $ip = $_REQUEST['ip'];
   $count = $_REQUEST['count'];
   $size = $_REQUEST['size'];
-  $ex="traceroute $ip". ($count==30?"":" -m $count") . ($size=="5"?"":"-w $size");
+  $ex="traceroute ".escapeshellcmd($ip)."".($count==30?"":" -m ".escapeshellcmd($count)."").($size=="5"?"":"-w ".escapeshellcmd($size));
  break;
  case 3:{ $ex="route -n";
  break; }
- case 4:{ $ex=str_replace("\r","\n",$_REQUEST['cmd']);
+ case 4:{ $ex=str_replace("\r","\n",escapeshellcmd($_REQUEST['cmd']));
  break; }
 }
 
  $rname="/tmp/tmp.". str_pad(mt_rand(1000,9999), 4, "0", STR_PAD_LEFT)  .".sh";
  file_put_contents($rname,"#!/bin/bash\nexport PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'\n$ex\n");
- exec("bash $rname",$out);
+ exec("bash ".escapeshellcmd($rname),$out);
  header("Content-type: text/plain");
  echo (unlink($rname)?"":"There was an error when trying to delete the file $rname.\n") . implode("\n",$out);
 ?>
